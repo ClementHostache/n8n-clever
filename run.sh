07a5443695fa7fc5336cb08c28b49ad9b07eb8aa
ClementHostache <101290460+ClementHostache@users.noreply.github.com>
2026-06-26 21:19:28 +0200

    Update run.sh retiré le start

--- run.sh
+++ run.sh
@@ -2,39 +2,26 @@
 
 set -e
 
-# set -x
+export N8N_PORT="$PORT"
+export N8N_PROTOCOL="https"
 
-export N8N_PORT=$PORT
+export DB_TYPE="postgresdb"
+export DB_POSTGRESDB_DATABASE="$POSTGRESQL_ADDON_DB"
+export DB_POSTGRESDB_HOST="$POSTGRESQL_ADDON_HOST"
+export DB_POSTGRESDB_PORT="$POSTGRESQL_ADDON_PORT"
+export DB_POSTGRESDB_USER="$POSTGRESQL_ADDON_USER"
+export DB_POSTGRESDB_PASSWORD="$POSTGRESQL_ADDON_PASSWORD"
 
-export N8N_PROTOCOL=https
-
-export DB_TYPE=postgresdb
-
-export DB_POSTGRESDB_DATABASE=$POSTGRESQL_ADDON_DB
-
-export DB_POSTGRESDB_HOST=$POSTGRESQL_ADDON_HOST
-
-export DB_POSTGRESDB_PORT=$POSTGRESQL_ADDON_PORT
-
-export DB_POSTGRESDB_USER=$POSTGRESQL_ADDON_USER
-
-export DB_POSTGRESDB_PASSWORD=$POSTGRESQL_ADDON_PASSWORD
-
-export GENERIC_TIMEZONE="UTC"export GENERIC_TIMEZONE="Europe/Paris"
+export GENERIC_TIMEZONE="Europe/Paris"
 export TZ="Europe/Paris"
 
-if [ -z "$N8N_HOST" ]
+export N8N_LISTEN_ADDRESS="${N8N_LISTEN_ADDRESS:-0.0.0.0}"
 
-then
-
- export N8N_HOST=$(echo "$APP_ID" | tr '_' '-').cleverapps.io
-
+if [ -z "$N8N_HOST" ]; then
+  export N8N_HOST="$(echo "$APP_ID" | tr '_' '-').cleverapps.io"
 fi
 
 echo "Host: $N8N_HOST"
+echo "Port: $N8N_PORT"
 
-# env
-
-./node_modules/.bin/n8n start
-
-exit 1
+exec ./node_modules/.bin/n8n
